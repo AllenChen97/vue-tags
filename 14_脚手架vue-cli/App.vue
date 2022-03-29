a29c30d23f3454002a582452d76b35958d1f497f
JinzhaoChen <j.chen88@newcastle.ac.uk>
2022-03-29 17:35:39 +0100

    first commit

---
+++ 14_脚手架vue-cli/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="./assets/logo.png" alt="logo">
+        <school></school>
+    </div>
+</template>
+
+<script>
+    Vue.config.productionTip = false;
+
+    import School from './School
